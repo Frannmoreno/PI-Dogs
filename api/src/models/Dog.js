22c701234bc0581844c949a59cef10805d93264a
Franco Nahuel Moreno <morenofranco1999@gmail.com>
2022-08-30 14:48:02 -0300

    Tablas Dog finalizadas

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,31 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    heigth: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+
+    weight: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 };
