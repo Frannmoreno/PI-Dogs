57f285ee8b10dfe1bcf782b642a0cd25b0abee1c
Franco Nahuel Moreno <morenofranco1999@gmail.com>
2022-08-31 19:46:35 -0300

    route dogs finalizada

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,21 +1,36 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-var express = require('express')
-const controllers = require('../Controllers/Controllers')
+var express = require('express');
+const { getAllDogs } = require('../Controllers/Controllers');
+
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.get('/dogs', async (req,res) => {
-    try {
-        return res.status(200).json(controllers.getAllDogs())
-    } catch(error) {
-        return res.status(404).json({error: 'No se pudieron mostrar los perros'})
+    const { name } = req.query 
+    if (name) {
+        let allDogs = await getAllDogs()
+        let dogName = await allDogs.filter(dog => dog.name.toLowerCase().includes(name.toLowerCase()))
+        if (dogName.length){
+            return res.status(200).json(dogName)
+        } else {
+            return res.status(404).json({error: 'The dog in nos avaleable'})
     }
+  }
+  return res.status(200).json(getAllDogs())
 })
 
+
+router.get('/dog?name', async(req,res) => {
+    try {
+
+    } catch (error) {
+
+    }
+})
 
 router.post('/dogs',(req,res) => {
     try{
