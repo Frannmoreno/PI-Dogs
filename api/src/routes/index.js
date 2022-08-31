4f05f226f21a182673712c0f6f2b82b8f9d80dcb
Franco Nahuel Moreno <morenofranco1999@gmail.com>
2022-08-31 10:20:44 -0300

    index 1ra ruta

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,28 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+var express = require('express')
+const controllers = require('../Controllers/Controllers')
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get('/dogs', async (req,res) => {
+    try {
+        return res.status(200).json(controllers.getAllDogs())
+    } catch(error) {
+        return res.status(404).json({error: 'No se pudieron mostrar los perros'})
+    }
+})
+
+
+router.post('/dogs',(req,res) => {
+    try{
+
+    } catch (error) {
+
+    }
+})
 
 module.exports = router;
