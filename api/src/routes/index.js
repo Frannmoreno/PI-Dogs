0fe1fdae5858de9c7dec48db5593990638c58c70
Franco Nahuel Moreno <morenofranco1999@gmail.com>
2022-09-02 15:45:06 -0300

    ruta /temperaments terminada

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,7 +4,7 @@ const { getAllDogs } = require('../Controllers/Controllers')
 // Ejemplo: const authRouter = require('./auth.js');
 const { Dog, Temperaments} = require('../db')
 const router = Router();
-const axios = require('axios')
+const axios = require('axios');
 
 
 // Configurar los routers
@@ -17,23 +17,42 @@ router.get('/dogs', async (req,res) => {
         if(name) {
             const dogSelected = allDogs.filter((dog) => dog.name.toLowerCase().includes(name.toLowerCase()))
             if (dogSelected.length){
-                return res.status(201).send(dogSelected)
+                return res.status(200).send(dogSelected)
             } else {
                 return res.status(404).send({error: 'The dog is at the park'})
             }
         } else {
-            return res.status(200).json(allDogs)
+            return res.status(201).json(allDogs)
         }
     })
 
 
-router.get('/dog?name', async(req,res) => {
+router.get('/temperaments', async (req,res) => {
     try {
-
-    } catch (error) {
-
+        const dataApi = await axios.get('https://api.thedogapi.com/v1/breeds');
+        const temperaments = await dataApi.data?.map(temp => temp.temperament) // obtengo todos los temperamentos
+        const tempOrd = temperaments.join(',').split(',').sort()
+        const newsTemps = new Set(tempOrd)
+        newsTemps.forEach(el => {
+            if(el.length >= 1) {
+                Temperaments.findOrCreate({
+                    where: {name: el}
+                })
+            } else null
+        })
+        const entregoDB = await Temperaments.findAll()
+        res.status(202).json(entregoDB)
+    }catch (error){
+        res.status(404).send({error: 'There are not temperaments'})
     }
+   
 })
+
+
+
+
+
+
 
 router.post('/dogs',(req,res) => {
     try{
