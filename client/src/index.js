0d551b90551dac5da53ffe1396e888619b412038
Franco Nahuel Moreno <morenofranco1999@gmail.com>
2022-09-06 03:01:57 -0300

    Implementacion de las Routes

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/Store/index'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById('root')
 );
 
